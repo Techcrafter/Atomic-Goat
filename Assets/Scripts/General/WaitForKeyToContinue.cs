d8f7dc2105dfb9466f7fadb0a93bd038a9a6c785
Techcrafter <skyboyskyman@gmail.com>
2020-04-03 17:39:35 +0200

    Added an intro to the game and redesigned the website a bit.

---
+++ Assets/Scripts/General/WaitForKeyToContinue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForKeyToContinue : MonoBehaviour
+{
+	GameObject LoadingText;
+	
+	public int SceneToLoad;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		LoadingText = GameObject.Find("LoadingText");
+		LoadingText.SetActive(false);
+	}
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Submit") || Input.GetButtonDown("Cancel") || Input.GetButtonDown("Jump"))
+		{
+			LoadingText.SetActive(true);
+			Application.LoadLevel(SceneToLoad);
+		}
+    }
+}
